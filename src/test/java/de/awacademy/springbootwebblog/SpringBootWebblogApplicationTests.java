527a88dcf3f94ef1693223b14f8944d639441971
Manu-Ela <67741199+Manu-Ela@users.noreply.github.com>
2020-10-15 09:31:10 +0200

    Update SpringBootWebblogApplicationTests.java
    add simple unit test

--- src/test/java/de/awacademy/springbootwebblog/SpringBootWebblogApplicationTests.java
+++ src/test/java/de/awacademy/springbootwebblog/SpringBootWebblogApplicationTests.java
@@ -3,6 +3,7 @@ package de.awacademy.springbootwebblog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/* 
 @SpringBootTest
 class SpringBootWebblogApplicationTests {
 
@@ -10,4 +11,12 @@ class SpringBootWebblogApplicationTests {
 	void contextLoads() {
 	}
 
+}
+*/
+
+@Test
+public void simpleTest() {
+	int a=1;
+	Assert.assert.Equals(1, a);
+		
 }

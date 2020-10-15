ed10f8ee6701b6c8cfc12d9e86cd446fd520c26a
Ralfn71 <67752644+Ralfn71@users.noreply.github.com>
2020-10-15 09:51:40 +0200

    Update SpringBootWebblogApplicationTests.java

--- src/test/java/de/awacademy/springbootwebblog/SpringBootWebblogApplicationTests.java
+++ src/test/java/de/awacademy/springbootwebblog/SpringBootWebblogApplicationTests.java
@@ -9,26 +9,7 @@ class SpringBootWebblogApplicationTests {
 	@Test
 	public void simpleTest() {
 		int a=1;
-		Assert.assertEquals(1, a);
-
-	}
-	
-	@Test
-	public void simpleTest2() {
-		int a=1;
-		Assert.assertEquals(1, a);
-
-	}
-	@Test
-	public void simpleTest3() {
-		int a=1;
-		Assert.assertEquals(1, a);
-
-	}
-	@Test
-	public void simpleTest4() {
-		int a=1;
-		Assert.assertEquals(1, a);
+		assertEquals(1, a);
 
 	}
 }

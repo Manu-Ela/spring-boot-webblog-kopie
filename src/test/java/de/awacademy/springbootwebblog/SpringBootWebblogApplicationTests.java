89cdaa8a6b46ab5d7c42ec351478b4812bec94e7
Ralfn71 <67752644+Ralfn71@users.noreply.github.com>
2020-10-15 09:59:52 +0200

    Update SpringBootWebblogApplicationTests.java

--- src/test/java/de/awacademy/springbootwebblog/SpringBootWebblogApplicationTests.java
+++ src/test/java/de/awacademy/springbootwebblog/SpringBootWebblogApplicationTests.java
@@ -1,9 +1,8 @@
 package de.awacademy.springbootwebblog;
 
-import org.junit.Test;
 import org.junit.Assert;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import org.junit.Assert.assertEquals;
+import org.junit.Assert.assertTrue;
 
 
 class SpringBootWebblogApplicationTests {

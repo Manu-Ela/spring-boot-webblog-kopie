b0ebd4aeaa36c267aa0bb36dbc95970534b77384
Ralfn71 <67752644+Ralfn71@users.noreply.github.com>
2020-10-15 09:39:15 +0200

    fix tests

--- src/test/java/de/awacademy/springbootwebblog/SpringBootWebblogApplicationTests.java
+++ src/test/java/de/awacademy/springbootwebblog/SpringBootWebblogApplicationTests.java
@@ -19,26 +19,26 @@ class SpringBootWebblogApplicationTests {
 	@Test
 	public void simpleTest() {
 		int a=1;
-		Assert.assert.Equals(1, a);
+		Assert.assertEquals(1, a);
 
 	}
 	
 	@Test
 	public void simpleTest2() {
 		int a=1;
-		Assert.assert.Equals(1, a);
+		Assert.assertEquals(1, a);
 
 	}
 	@Test
 	public void simpleTest3() {
 		int a=1;
-		Assert.assert.Equals(1, a);
+		Assert.assertEquals(1, a);
 
 	}
 	@Test
-	public void simpleTest3() {
+	public void simpleTest() {
 		int a=1;
-		Assert.assert.Equals(1, a);
+		Assert.assertEquals(1, a);
 
 	}
 }

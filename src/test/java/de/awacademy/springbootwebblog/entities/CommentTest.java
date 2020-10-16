7bebc8cb83a61682bf78fc2c04cf73ec38798dd4
Manuela Raschke <manuela.raschke@awacademyconsultant.de>
2020-10-16 10:20:12 +0200

    fix unit test comment

--- src/test/java/de/awacademy/springbootwebblog/entities/CommentTest.java
+++ src/test/java/de/awacademy/springbootwebblog/entities/CommentTest.java
@@ -1,19 +1,22 @@
-package de.awacademy.springbootwebblog.services;
+package de.awacademy.springbootwebblog.entities;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CommentTest {
     @Test
 	public void name() {
 		
 		// 1. Testdaten vorbereiten
-		Comment comment = new Comment(1); // id = 1
-		comment.setName("Test"); 
+		Comment comment = new Comment("TestText", "TestName"); 
 		
 		// 2. Logik ausfuehren
-		String name = comment.name();
+		String name = comment.getName();
 		
 		// 3. Erwartungswerte pruefen
-		Assert.assertEquals("Test", name);
+		assertEquals("Test", name);
 		
 	}
 
